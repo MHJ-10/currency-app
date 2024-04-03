a8e397f75941b0fbc096ee1a91a529bf1e6ac941
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-04-03 13:06:20 +0330

    responsive home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
+import heroBg from "@/public/images/landing-img.png";
+import coinLogo from "@/public/images/bitcoin-logo.png";
+import Image from "next/image";
+
 export default function Home() {
-  return <main className="">Home page</main>;
+  return (
+    <div className="text-white">
+      <div className="flex flex-col items-center justify-center gap-3 py-5 md:flex-row md:justify-around">
+        <div className="flex flex-col items-start justify-center gap-5 p-4 text-xl font-bold sm:text-2xl">
+          <p>
+            به دنیای{" "}
+            <span className="bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-2xl text-transparent sm:text-3xl">
+              تبادلات ارزی
+            </span>{" "}
+            خوش آمدید
+          </p>
+          <p className="">
+            به سادگی تغییرات ساعتی در
+            <span className="bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-3xl text-transparent">
+              {" "}
+              بازار های مالی{" "}
+            </span>
+            را پیگیری کنید
+          </p>
+          <p>
+            از آخرین
+            <span className="bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-3xl text-transparent">
+              {" "}
+              تحولات بازار{" "}
+            </span>
+            آگاه شوید
+          </p>
+          <p className="text-sm font-thin">
+            برای دسترسی به جدول قیمت‌ها، به بخش مربوطه در وبسایت ما مراجعه کنید
+          </p>
+        </div>
+        <div className="lg:animate-spin">
+          <Image src={coinLogo} alt="bit-coin" />
+        </div>
+      </div>
+      <div className="">
+        <Image src={heroBg} alt="hero-bg" priority width={1400} height={20} />
+      </div>
+    </div>
+  );
 }
