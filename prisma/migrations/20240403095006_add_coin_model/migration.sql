c3ecece4eefda210b8931098d2a35ba0883081ed
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-04-03 13:20:37 +0330

    setup prisma

---
+++ prisma/migrations/20240403095006_add_coin_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Coin` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` VARCHAR(191) NOT NULL,
+    `change` VARCHAR(191) NOT NULL,
+    `lowest` VARCHAR(191) NOT NULL,
+    `highest` VARCHAR(191) NOT NULL,
+    `time` VARCHAR(191) NOT NULL,
+    `status` ENUM('high', 'low', 'fixed') NOT NULL,
+    `updateTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
