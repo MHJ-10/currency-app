8590d1e2cbc06fe292da09f288764dc21862dac0
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-04-24 20:42:33 +0330

    change not found image

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="text-700 -mt-32 flex h-screen  flex-col items-center justify-center">
+    <div className="text-700 -mt-20 flex h-screen  flex-col items-center justify-center">
       <Image src={notFoundImg} alt="not-found" width={600} height={600} />
       <Link
-        className="rounded-md border-2 bg-slate-200 px-3 py-2 font-bold transition-colors duration-500 hover:border-slate-700 hover:bg-slate-300 hover:text-slate-700"
+        className="rounded-md bg-blue-600 px-3 py-2 font-bold text-white transition-colors duration-500 hover:border hover:border-white hover:bg-blue-700"
         href="/"
       >
         بازگشت به صفحه اصلی
