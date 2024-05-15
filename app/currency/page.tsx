ed25c6f28992cddf9d434ddb0a98681bc823c461
MHJ-10 <mhj10mhj10jafari@gmail.com>
2024-05-15 16:38:18 +0330

    fix table scroll

--- app/currency/page.tsx
+++ app/currency/page.tsx
@@ -16,34 +16,38 @@ const CurrencyPage = async () => {
 
   return (
     <table className="container mx-auto my-5 w-4/5 rounded-md border text-center text-white">
-      <thead className="border-b bg-white bg-opacity-70 text-primary">
-        <tr>
-          <th className="p-4">ارز آزاد</th>
-          <th className="p-4">قیمت زنده</th>
-          <th className="hidden p-4 sm:table-cell">تغییر</th>
-          <th className="hidden p-4 md:table-cell">کمترین</th>
-          <th className="hidden p-4 md:table-cell">بیشترین</th>
-          <th className="hidden p-4 sm:table-cell">زمان</th>
+      <thead className="flex w-full border-b bg-white bg-opacity-70 text-primary">
+        <tr className="flex w-full">
+          <th className="w-1/2 p-4 sm:w-1/4 md:w-1/6">ارز آزاد</th>
+          <th className="w-1/2 p-4 sm:w-1/4 md:w-1/6">قیمت زنده</th>
+          <th className="hidden p-4 sm:table-cell sm:w-1/4 md:w-1/6">تغییر</th>
+          <th className="hidden p-4 md:table-cell md:w-1/6">کمترین</th>
+          <th className="hidden p-4 md:table-cell md:w-1/6">بیشترین</th>
+          <th className="hidden p-4 sm:table-cell sm:w-1/4 md:w-1/6">زمان</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="flex h-[80vh] w-full flex-col items-center justify-between overflow-y-scroll">
         {currency.map((c) => (
           <tr
             key={c.name}
-            className={`border-b border-${c.status}-800 bg-${c.status}-700 transition-colors duration-500  hover:bg-opacity-80`}
+            className={`flex border-b border-${c.status}-800 bg-${c.status}-700 w-full transition-colors duration-500  hover:bg-opacity-80`}
           >
-            <td className="whitespace-nowrap p-4">{c.name}</td>
-            <td className="whitespace-nowrap p-4">{c.price}</td>
-            <td className="hidden whitespace-nowrap p-4 sm:table-cell">
+            <td className="w-1/2 whitespace-nowrap p-4 sm:w-1/4 md:w-1/6">
+              {c.name}
+            </td>
+            <td className="w-1/2 whitespace-nowrap p-4 sm:w-1/4 md:w-1/6">
+              {c.price}
+            </td>
+            <td className="hidden whitespace-nowrap p-4 sm:table-cell sm:w-1/4 md:w-1/6">
               {c.change}
             </td>
-            <td className="hidden whitespace-nowrap p-4 md:table-cell">
+            <td className="hidden whitespace-nowrap p-4 md:table-cell md:w-1/6">
               {c.lowest}
             </td>
-            <td className="hidden whitespace-nowrap p-4 md:table-cell">
+            <td className="hidden whitespace-nowrap p-4 md:table-cell md:w-1/6">
               {c.highest}
             </td>
-            <td className="hidden whitespace-nowrap p-4 sm:table-cell">
+            <td className="hidden whitespace-nowrap p-4 sm:table-cell sm:w-1/4 md:w-1/6">
               {c.time}
             </td>
           </tr>
